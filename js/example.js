30606a00079b607fe4e7fbff46bb2e08d48ce2bb
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-12-26 12:18:37 +0100

    Add card counting logic to cc function

--- js/example.js
+++ js/example.js
@@ -1,7 +1,27 @@
-function isLess(a, b) {
+let count = 0;
+
+function cc(card) {
   // Only change code below this line
-  return a < b;
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      break;
+    default:
+      count--;
+      break;
+  }
+  return `${count} ${count > 0 ? "Bet" : "Hold"}`;
   // Only change code above this line
 }
 
-isLess(10, 15);
+cc(2); //
+cc(2); //
+cc(4); // какой результат?
