21cce7ebb50740a2df46ca50735b8e109dca1551
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-12-26 12:37:30 +0100

    Refactor object properties in example.js

--- js/example.js
+++ js/example.js
@@ -1,6 +1,10 @@
-const myDog = {
-  name: "Rex",
-  legs: 4,
-  tails: 1,
-  friends: ["everything!"],
+// Setup
+const testObj = {
+  hat: "ballcap",
+  shirt: "jersey",
+  shoes: "cleats",
 };
+
+// Only change code below this line
+const hatValue = testObj.hat; // Change this line
+const shirtValue = testObj.shirt; // Change this line
