306dec7fd85f899cce35e77289d755b855c07374
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-12-26 12:33:47 +0100

    Refactor code: Replace count variable with myDog object

--- js/example.js
+++ js/example.js
@@ -1,27 +1,6 @@
-let count = 0;
-
-function cc(card) {
-  // Only change code below this line
-  switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 7:
-    case 8:
-    case 9:
-      break;
-    default:
-      count--;
-      break;
-  }
-  return `${count} ${count > 0 ? "Bet" : "Hold"}`;
-  // Only change code above this line
-}
-
-cc(2); //
-cc(2); //
-cc(4); // какой результат?
+const myDog = {
+  name: "Rex",
+  legs: 4,
+  tails: 1,
+  friends: ["everything!"],
+};
