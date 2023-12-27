fd969c6e495c2590f19464eab85749d302621d65
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-12-27 21:21:27 +0100

    Refactor myMusic array to myStorage object

--- js/example.js
+++ js/example.js
@@ -1,15 +1,13 @@
-const myMusic = [
-  {
-    artist: "Billy Joel",
-    title: "Piano Man",
-    release_year: 1973,
-    formats: ["CD", "8T", "LP"],
-    gold: true,
+const myStorage = {
+  car: {
+    inside: {
+      "glove box": "maps",
+      "passenger seat": "crumbs",
+    },
+    outside: {
+      trunk: "jack",
+    },
   },
-  {
-    artist: "Billy Joel",
-    title: "The Stranger",
-    release_year: 1977,
-    formats: ["CD", "8T", "LP"],
-  },
-];
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
