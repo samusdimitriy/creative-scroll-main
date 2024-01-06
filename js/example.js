6e7e86b5e93ffeccdb0060e9846fa564f4349c39
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2024-01-06 10:42:19 +0100

    Refactor example.js: Remove unnecessary code and fix potential infinite loop

--- js/example.js
+++ js/example.js
@@ -1,45 +1,12 @@
-// Налаштування
-const recordCollection = {
-  2548: {
-    albumTitle: "Slippery When Wet",
-    artist: "Bon Jovi",
-    tracks: ["Let It Rock", "You Give Love a Bad Name"],
-  },
-  2468: {
-    albumTitle: "1999",
-    artist: "Prince",
-    tracks: ["1999", "Little Red Corvette"],
-  },
-  1245: {
-    artist: "Robert Palmer",
-    tracks: [],
-  },
-  5439: {
-    albumTitle: "ABBA Gold",
-  },
-};
+// Setup
+const myArray = [];
 
-// Змініть код лише під цим рядком
-function updateRecords(records, id, prop, value) {
-  if (value !== "") {
-    if (prop === "tracks") {
-      if (!records[id].hasOwnProperty("tracks")) {
-        records[id][prop] = [value];
-      } else {
-        records[id][prop].push(value);
-      }
-    } else {
-      records[id][prop] = value;
-    }
-  } else {
-    if (prop === "tracks" && records[id].hasOwnProperty("tracks")) {
-      delete records[id][prop];
-    } else {
-      delete records[id][prop];
-    }
-  }
-
-  return records;
+// Only change code below this line
+let i = 5;
+while (i >= 0) {
+  myArray.push(i);
+  i--;
 }
+console.log(myArray);
 
-updateRecords(recordCollection, 5439, "artist", "ABBA");
+// Potential infinite loop detected on line 6. Tests may fail if this is not changed
